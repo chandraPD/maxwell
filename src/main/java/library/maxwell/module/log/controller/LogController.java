8049e3746a5a4e4519be1b164043050f1b734901
Mochamad Farid Naufal <mochamad.farid.n@outlook.com>
2020-12-24 17:22:38 +0700

    merubah anotasi di logController

--- src/main/java/library/maxwell/module/log/controller/LogController.java
+++ src/main/java/library/maxwell/module/log/controller/LogController.java
@@ -4,12 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import library.maxwell.module.log.dto.LogDto;
@@ -17,7 +17,7 @@ import library.maxwell.module.log.entity.LogEntity;
 import library.maxwell.module.log.repository.LogRepository;
 
 @RestController
-@Transactional
+@RequestMapping("/log")
 public class LogController {
 	@Autowired
 	private LogRepository logRepository;
@@ -43,6 +43,7 @@ public class LogController {
 		LogEntity logEntity = convertToLogEntity(dto);
 		return ResponseEntity.ok(logEntity);
 	}
+	
 	//UPDATE
 	@PutMapping("/update-log/{idLog}")
 	public ResponseEntity<?> updateLog(@PathVariable Integer idLog,
