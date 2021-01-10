5ca0886edcfe2dbd462a987f1e67429bd0840aba
Mochamad Farid Naufal <mochamad.farid.n@outlook.com>
2021-01-10 11:56:10 +0700

    menambahkan end point get-log-lastActivity untuk get data log terbaru

--- src/main/java/library/maxwell/module/log/service/LogService.java
+++ src/main/java/library/maxwell/module/log/service/LogService.java
@@ -2,6 +2,8 @@ package library.maxwell.module.log.service;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
+
 import library.maxwell.config.security.auth.UserPrincipal;
 import library.maxwell.module.log.dto.LogDto;
 import library.maxwell.module.log.entity.LogEntity;
@@ -9,6 +11,7 @@ import library.maxwell.module.log.entity.LogEntity;
 public interface LogService {
 	List<LogEntity> getAllLog();
 	LogEntity getLogById(Integer idLog);
+	ResponseEntity<?> getLogLastActivity();
 	LogEntity addLog(UserPrincipal userPrincipal, LogDto dto);
 	LogEntity updateLog(Integer idLog, LogDto dto);
 	LogEntity deleteLog(Integer idLog);
