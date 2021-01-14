0e22dbd07e6e0f3ebe1de4aeb7d1c053a65ae310
mdayann <mdayan312@gmail.com>
2021-01-14 11:44:15 +0700

    handle password

--- src/main/java/library/maxwell/module/user/dto/StatusMessageDto.java
+++ src/main/java/library/maxwell/module/user/dto/StatusMessageDto.java
@@ -1,12 +1,15 @@
 package library.maxwell.module.user.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class StatusMessageDto<T> {
 
 
@@ -15,4 +18,19 @@ public class StatusMessageDto<T> {
     private String messages;
 
     private T data;
+
+    public static <T> StatusMessageDto error(String message) {
+        return StatusMessageDto.builder()
+                .status(HttpStatus.BAD_GATEWAY.value())
+                .messages(message)
+                .build();
+    }
+
+    public static <T> StatusMessageDto success(String message, T data) {
+        return StatusMessageDto.builder()
+                .status(HttpStatus.OK.value())
+                .messages(message)
+                .data(data)
+                .build();
+    }
 }
